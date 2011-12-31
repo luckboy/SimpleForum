bc0b50891a6ca15d02881846ff11062d38693690
Lukasz Szpakowski <luckboy@vp.pl>
2011-12-31 16:46:05 +0100

    Opcja :dependent.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
   belongs_to :forum
-  has_many :posts, :order => "created_at ASC"
+  has_many :posts, :order => "created_at ASC", :dependent => :destroy
 
   validates :subject, :presence => true
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  has_many :topics, :foreign_key => "author_id"
-  has_many :posts, :foreign_key => "author_id"  
+  has_many :topics, :foreign_key => "author_id", :dependent => :nullify
+  has_many :posts, :foreign_key => "author_id", :dependent => :nullify
   
   def admin?
     is_admin
